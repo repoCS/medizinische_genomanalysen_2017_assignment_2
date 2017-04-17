0b2b85fc8a5967154c063f39647ec3b7b594c8ce
vortex <register.mybin@gmail.com>
2017-04-17 16:59:39 +0200

    auto commit

--- assignment2_CS.py
+++ assignment2_CS.py
@@ -1,70 +1,90 @@
 #!/usr/bin/env python3
 
+'''
+Assignment 2 - Medizinische Genomanalyse
+von Clemens Spielvogel
+
+VOR DEM AUSFUEHREN muss der absolute Pfad der vcf-Datei fuer die Variable vcf (Zeile 13) angegeben werden!
+'''
+
 import vcf
 
 __author__ = 'Clemens Spielvogel'
+vcf_path = "/home/vortex/Bioinformatik/med_genomanalyse/AmpliseqExome.20141120.NA24385.vcf"
 
 class Assignment2:
-    def __init__(self):
+    def __init__(self, vcf_path):
         # Check if pyvcf is installed
         print("PyVCF version: {}".format(vcf.VERSION))
-        self.vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24385.vcf", "r"))
 
 
     def get_average_quality_of_son(self):
-        self.sum = 0
-        self.count = 0
-        for record in self.vcf:
-            self.sum = self.sum + record.QUAL
-            self.count += 1
-        print(float(self.sum) / self.count)
-        return float(self.sum) / self.count
+        print("\n---------------\nAverage quality of son:")
+
+        sum = 0
+        count = 0
+        for record in vcf.Reader(open(vcf_path, "r")):
+            sum = sum + record.QUAL
+            count += 1
+        print(round(float(sum) / count, 2))
+        return float(sum) / count
 
 
     def get_total_number_of_variants_of_son(self):
-        for record in self.vcf:
-            self.count += 1
-        print(self.count)
-        return self.count
+        print("\n---------------\nNumber of variants (son):")
+
+        count = 0
+        for record in vcf.Reader(open(vcf_path, "r")):
+            count += 1
+        print(count)
+        return count
 
 
     def get_variant_caller_of_vcf(self):
-        for line in self.vcf._header_lines:
+        for line in vcf.Reader(open(vcf_path, "r"))._header_lines:
              if line.startswith("##source"):
                  return "-", str.split(line, '"')[1]
 
 
     def get_human_reference_version(self):
-        for line in self.vcf._header_lines:
+        print("\n---------------\nHuman reference version:")
+
+        for line in vcf.Reader(open(vcf_path, "r"))._header_lines:
              if line.startswith("##reference=file://"):
                  print(str.split(line, '/')[6])
                  return str.split(line, '/')[6]
 
 
     def get_number_of_indels(self):
-        self.indels = 0
-        for record in self.vcf:
+        print("\n---------------\nNumber of indels:")
+
+        indels = 0
+        for record in vcf.Reader(open(vcf_path, "r")):
             if record.is_indel:
-                self.indels += 1
-        print(self.indels)
-        return self.indels
+                indels += 1
+        print(indels)
+        return indels
 
 
     def get_number_of_snvs(self):
-        self.snv = 0
-        for record in self.vcf:
+        print("\n---------------\nNumber of SNVs:")
+
+        snv = 0
+        for record in vcf.Reader(open(vcf_path, "r")):
             if record.is_snp:
-              self.snv += 1
-        print(self.snv)
-        return self.snv
+              snv += 1
+        print(snv)
+        return snv
 
 
     def get_number_of_heterozygous_variants(self):
-        self.num_hetero = 0
-        for record in self.vcf:
-            self.num_hetero += record.num_hetero
-        print(self.num_hetero)
-        return self.num_hetero
+        print("\n---------------\nNumber of heterozygous variants:")
+
+        num_hetero = 0
+        for record in vcf.Reader(open(vcf_path, "r")):
+            num_hetero += record.num_het
+        print(num_hetero)
+        return num_hetero
 
 
     def print_summary(self):
@@ -72,12 +92,12 @@ class Assignment2:
         self.get_total_number_of_variants_of_son()
         self.get_variant_caller_of_vcf()
         self.get_human_reference_version()
-        self.get_number_of_indels()                   # 1823
-        self.get_number_of_snvs()                     # 36703
-        self.get_number_of_heterozygous_variants()    # 23819
+        self.get_number_of_indels()
+        self.get_number_of_snvs()
+        self.get_number_of_heterozygous_variants()
 
 
 if __name__ == '__main__':
     print("Assignment 2")
-    assignment1 = Assignment2()
+    assignment1 = Assignment2(vcf_path)
 assignment1.print_summary()
